5fa73d6d319a17965a232875b4416f4d730fdac3
usman_sol <usman.liaqut@unitedsol.net>
2023-08-25 18:01:09 +0500

    changes in seach result page

--- src/Components/Blocks/TermsConditions/TermsSidebar.js
+++ src/Components/Blocks/TermsConditions/TermsSidebar.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function TermsSidebar({TcSidebar}) {
+  const [selectedItem, setSelectedItem] = useState(null)
+
+    const handleScroll = (id) => {
+        setSelectedItem(id);
+    }
   return (
     <div className="tc_Sidebar">
           {
                TcSidebar.map((items)=>
 
 
-                  <div className='sidebar_inner' key={items.id}>
+                  <div className='sidebar_inner' key={items.id} onClick={() => handleScroll(items.id)}>
                
                      <p className='primary_text'>  
                        <Link href={'#!'} to={`cms_${items.id}`} smooth={true} offset={-50} activeClass="cms_tab_active" spy={true}> {items.title}
